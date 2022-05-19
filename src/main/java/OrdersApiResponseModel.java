c41f3cf74de5dfc3c98c7d8cbf5f538777914b19
Lex Lobov <lexlobov@yandex.ru>
2022-05-19 17:28:18 +0700

    небольшие правки

--- src/main/java/OrdersApiResponseModel.java
+++ src/main/java/OrdersApiResponseModel.java
@@ -1,14 +1,16 @@
 import java.util.List;
 
 public class OrdersApiResponseModel {
+    private boolean success;
     private List<OrderModel> orders;
     private long total;
     private long totalToday;
 
-    public OrdersApiResponseModel(List<OrderModel> orders, long total, long totalToday) {
+    public OrdersApiResponseModel(boolean success, List<OrderModel> orders, long total, long totalToday) {
         this.orders = orders;
         this.total = total;
         this.totalToday = totalToday;
+        this.success = success;
     }
 
     public List<OrderModel> getOrders() {
@@ -21,5 +23,9 @@ public class OrdersApiResponseModel {
 
     public long getTotalToday() {
         return totalToday;
+    }
+
+    public boolean isSuccess(){
+        return success;
     }
 }
