0fd53afef53a4fa607f8680f3ff1e0f38584678d
Lex Lobov <lexlobov@yandex.ru>
2022-05-19 14:21:11 +0700

    добавил модели пользователя и ответов на запросы

---
+++ src/main/java/StandrdApiResponseModel.java
@@ -0,0 +1,15 @@
+public class StandrdApiResponseModel {
+    private boolean success;
+    private String message;
+
+    public StandrdApiResponseModel() {
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/UserLoginSuccessApiResponseModel.java
@@ -0,0 +1,32 @@
+public class UserLoginSuccessApiResponseModel {
+    private boolean success;
+    private String accessToken;
+    private String refreshToken;
+    private  UserModel user;
+
+    public UserLoginSuccessApiResponseModel(boolean success, String accessToken, String refreshToken, UserModel user) {
+        this.success = success;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.user = user;
+    }
+
+    public UserLoginSuccessApiResponseModel() {
+    }
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public UserModel getUser() {
+        return user;
+    }
+}
