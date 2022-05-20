140fea883912d54d71aede391ed356654351daa1
Lex Lobov <lexlobov@yandex.ru>
2022-05-20 13:52:44 +0700

    Добавил клиент для создания пользователя

---
+++ src/main/java/client/CreateUserClient.java
@@ -0,0 +1,18 @@
+package client;
+
+import io.restassured.response.ValidatableResponse;
+import model.UserModel;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateUserClient extends BaseHttpConfig {
+
+    private final String createUserUri = "/api/auth/register";
+    public ValidatableResponse createUser(UserModel user){
+        return given().spec(baseSpec())
+                .body(user)
+                .when()
+                .post(createUserUri)
+                .then();
+    }
+}
