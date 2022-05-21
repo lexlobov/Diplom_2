c3e39e9304ebe744aae902ffb1279cef8109f31d
LexLobov <lexlobov@yandex.ru>
2022-05-21 14:03:14 +0700

    Обновил Steps

--- src/test/java/UserSteps.java
+++ src/test/java/UserSteps.java
@@ -1,10 +1,13 @@
 import client.CreateUserClient;
+import client.DeleteUserClient;
 import client.LoginUserClient;
+import client.UpdateUserClient;
 
 public class UserSteps {
 
     CreateUserClient createUserClient = new CreateUserClient();
     LoginUserClient loginUserClient = new LoginUserClient();
-
+    UpdateUserClient updateUserClient = new UpdateUserClient();
+    DeleteUserClient deleteUserClient = new DeleteUserClient();
 
 }
