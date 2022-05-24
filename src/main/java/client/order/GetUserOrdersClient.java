2a535d8f0636e014fe691d223019a2dd26f55b40
LexLobov <lexlobov@yandex.ru>
2022-05-24 14:54:22 +0700

    Добавил клиент для получения списка заказов конкретного пользователя

---
+++ src/main/java/client/order/GetUserOrdersClient.java
@@ -0,0 +1,21 @@
+package client.order;
+
+import client.BaseHttpConfig;
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.given;
+
+public class GetUserOrdersClient extends BaseHttpConfig {
+
+    private final String getUserOrdersUri = "/api/orders";
+
+    private ValidatableResponse getUserOrders(String authToken){
+        return given().spec(baseSpec())
+                .queryParam("token", authToken)
+                .when()
+                .get(getUserOrdersUri)
+                .then();
+
+    }
+
+}
