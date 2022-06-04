cfa5f2bae02a0fd7b79ea737f132a93f9f5cb71d
Alex Lobov <lexlobov@yandex.ru>
2022-06-04 15:40:20 +0700

    Вынес создание и удаление пользователя в Before и After

--- src/test/java/user/UserLoginTest.java
+++ src/test/java/user/UserLoginTest.java
@@ -3,6 +3,8 @@ package user;
 import com.github.javafaker.Faker;
 import io.qameta.allure.junit4.DisplayName;
 import model.UserModel;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 @DisplayName("Различные сценарии авторизации")
@@ -17,43 +19,43 @@ public class UserLoginTest {
 
     UserModel user = new UserModel(email, password, name);
 
+    @Before
+    public void setUp(){
+        steps.createNewUser(user);
+    }
+
+    @After
+    public void cleanUp(){
+        steps.deleteUser();
+    }
+
     @Test
     @DisplayName("Авторизация пользователя, позитивный сценарий")
     public void userLoginPositiveTest(){
-        steps.createNewUser(user);
         steps.loginUserPositive(email, password);
-        steps.deleteUser();
     }
 
     @Test
     @DisplayName("Авторизация пользователя, негативный сценарий с незаполненным логином")
     public void userLoginWithoutEmailNegativeTest(){
-        steps.createNewUser(user);
         steps.loginUserNegative(null, password);
-        steps.deleteUser();
     }
 
     @Test
     @DisplayName("Авторизация пользователя, негативный сценарий с незаполненным паролем")
     public void userLoginWithoutPasswordNegativeTest(){
-        steps.createNewUser(user);
         steps.loginUserNegative(email, null);
-        steps.deleteUser();
     }
 
     @Test
     @DisplayName("Авторизация пользователя, негативный сценарий с неверным паролем")
     public void userLoginWithWrongPasswordNegativeTest(){
-        steps.createNewUser(user);
         steps.loginUserNegative(email, faker.lorem().characters(10, true));
-        steps.deleteUser();
     }
 
     @Test
     @DisplayName("Авторизация пользователя, негативный сценарий с неверным email")
     public void userLoginWithWrongEmailNegativeTest(){
-        steps.createNewUser(user);
         steps.loginUserNegative(faker.name().username() + "@maik.ru", password);
-        steps.deleteUser();
     }
 }
